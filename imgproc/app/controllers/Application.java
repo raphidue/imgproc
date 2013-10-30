23023778f0dfdad8b94e932cf41ee3ceb5d7fdcc
raphidue <nguyenthaiduy90@googlemail.com>
2013-10-30 10:12:54 +0100

    added processing active buttons

--- imgproc/app/controllers/Application.java
+++ imgproc/app/controllers/Application.java
@@ -7,24 +7,24 @@ import views.html.*;
 
 public class Application extends Controller {
     
-    public static Result index() {
-        return ok(views.html.index.render(scripts.render(), navigation.render("index"), footer.render()));
-    }
+	public static Result index() {
+		return ok(views.html.index.render(scripts.render(), navigation.render("index"), footer.render()));
+	}
     
-    public static Result processing() {
-        return ok(views.html.processing.render(scripts.render(), navigation.render("processing"), footer.render()));
-    }
+	public static Result processing() {
+		return ok(views.html.processing.render(scripts.render(), navigation.render("processing"), footer.render()));
+	}
     
-    public static Result contact() {        
-        return ok(views.html.contact.render(scripts.render(), navigation.render("contact"), footer.render()));
-    }
+	public static Result contact() {        
+		return ok(views.html.contact.render(scripts.render(), navigation.render("contact"), footer.render()));
+	}
     
-    public static Result about() {
-        return ok(views.html.about.render(scripts.render(), navigation.render("about"), footer.render()));
-    }
+	public static Result about() {
+		return ok(views.html.about.render(scripts.render(), navigation.render("about"), footer.render()));
+	}
     
-    public static Result documentation() {
-        return ok(views.html.documentation.render(scripts.render(), 
-            navigation.render("documentation"), footer.render()));
-    }
+	public static Result documentation() {
+		return ok(views.html.documentation.render(scripts.render(), 
+			navigation.render("documentation"), footer.render()));
+	}
 }
