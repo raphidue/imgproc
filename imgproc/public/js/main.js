6c85111bc3281896523f6d44118a18f659d6f782
raphidue <nguyenthaiduy90@googlemail.com>
2013-10-31 17:35:58 +0100

    docupage bug when clicking on links

--- imgproc/public/js/main.js
+++ imgproc/public/js/main.js
@@ -1,3 +1,8 @@
+$(function() {
+    var shiftWindow = function() { scrollBy(0, -75) };
+    if (location.hash) shiftWindow();
+    window.addEventListener("hashchange", shiftWindow);
+});
 /************************Processing Buttons Action*****************************/
 $(function() {
 	$("#showImg").click(function() {
