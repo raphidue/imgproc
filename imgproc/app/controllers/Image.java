5cefbba8203f9a1e021ab9e7f68f0cf5e0e0e805
raphidue <nguyenthaiduy90@googlemail.com>
2013-11-05 14:05:05 +0100

    added image controller

---
+++ imgproc/app/controllers/Image.java
@@ -0,0 +1,43 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html._include.*;
+import views.html.*;
+import play.mvc.Http.MultipartFormData;
+import play.mvc.Http.MultipartFormData.FilePart;
+import java.io.*;
+import play.*;
+
+public class Image extends Controller {
+	
+	public static Result upload() {
+	  MultipartFormData body = request().body().asMultipartFormData();
+	  FilePart picture = body.getFile("picture");
+	  if (picture != null) {
+	    String fileName = picture.getFilename();
+	    String contentType = picture.getContentType(); 
+	    File file = picture.getFile();
+
+        String myUploadPath = Play.application().path().getAbsolutePath() + "/public/uploads/" + fileName;
+        file.renameTo(new File(myUploadPath));
+		
+		/*
+		File debug = new File("sql.txt");
+		try{
+			FileWriter writer = new FileWriter(debug);
+
+			writer.write(myUploadPath);
+
+			writer.close();
+		}catch(IOException e){
+		  e.printStackTrace();
+		}
+			*/
+	    return ok("File uploaded");
+	  } else {
+	    flash("error", "Missing file");
+	    return redirect(routes.Application.index());    
+	  }
+	}
+}
