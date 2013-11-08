c6f599af65f3aa02f8bc976fd7dd404c9167613a
raphidue <nguyenthaiduy90@googlemail.com>
2013-11-08 10:55:56 +0100

    updated create folder uploads if not exist

--- imgproc/app/controllers/Image.java
+++ imgproc/app/controllers/Image.java
@@ -12,19 +12,28 @@ import play.*;
 public class Image extends Controller {
 	
 	public static Result upload(String id) {
-	  MultipartFormData body = request().body().asMultipartFormData();
-	  FilePart picture = body.getFile("picture");
-	  if (picture != null) {
-	    String fileName = picture.getFilename();
-	    String contentType = picture.getContentType(); 
-	    File file = picture.getFile();
+		MultipartFormData body = request().body().asMultipartFormData();
+		FilePart picture = body.getFile("picture");
+		
+		File theDir = new File(Play.application().path().getAbsolutePath() + "/public" + uploads);
 
-        String myUploadPath = Play.application().path().getAbsolutePath() + "/public/uploads/" + id;
-        file.renameTo(new File(myUploadPath));
-	    return ok(views.html.image.render(id));
-	  } else {
-	    flash("error", "Missing file");
-	    return redirect(routes.Application.index());    
-	  }
+		// erstelle Ordner uploads wenn nicht existiert
+		if (!theDir.exists()) {
+			boolean result = theDir.mkdir();  
+		}
+
+	  
+		if (picture != null) {
+			String fileName = picture.getFilename();
+			String contentType = picture.getContentType(); 
+			File file = picture.getFile();
+
+			String myUploadPath = Play.application().path().getAbsolutePath() + "/public/uploads/" + id;
+			file.renameTo(new File(myUploadPath));
+			return ok(views.html.image.render(id));
+		} else {
+			flash("error", "Missing file");
+			return redirect(routes.Application.index());    
+		}
 	}
 }
