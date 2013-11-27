338415b60ec5d5ea88793ec3485c8cc95934840f
bfcm <ivapogor@htwg-konstanz.de>
2013-11-27 22:27:47 +0100

    Contact form validation

--- imgproc/public/js/contact.js
+++ imgproc/public/js/contact.js
@@ -6,6 +6,30 @@ $(function() {
 		var message = $('#inputMessage').val();
 		//console.log("Name: " + name + "    Email: " + email + "    Message: " + message);
 
+		var atpos=email.indexOf("@");
+		var dotpos=email.lastIndexOf(".");
+		if (atpos<1 || dotpos<atpos+2 || dotpos+2>=email.length){
+			//alert("Not a valid e-mail address");
+			$('#errorMessageEmail').removeClass("hidden");
+			$('#inputEmail').closest('div').addClass("has-error");
+			return;
+	  	} else {
+	  		$('#errorMessageEmail').addClass("hidden");
+	  		$('#inputEmail').closest('div').removeClass("has-error");
+	  	}
+
+	  	if (name == "")
+	  		name = "Anonymous";
+
+	  	if (message.length < 30){
+	  		$('#errorMessageMsg').removeClass("hidden");
+			$('#inputMessage').closest('div').addClass("has-error");
+			return;
+	  	} else {
+	  		$('#errorMessageMsg').addClass("hidden");
+	  		$('#inputMessage').closest('div').removeClass("has-error");
+	  	}
+
 		var json = {
 			"name": name,
 			"email": email,
@@ -14,8 +38,10 @@ $(function() {
 
 		jQuery.post("app_contact_sendEmail.html", json, function(data) {
 			//("Email sending status: " + data['status'])
+			$("#inputMessage").val("");
 			$('#myModal').modal();
 		}, "json");
+
 
 
 	});
