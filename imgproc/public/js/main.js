06ef7bc0ec9cdaeec172e2bf1dc13c6d9bb8dcf2
raphidue <nguyenthaiduy90@googlemail.com>
2013-12-19 15:36:13 +0100

    added hover event

--- imgproc/public/js/main.js
+++ imgproc/public/js/main.js
@@ -1,4 +1,28 @@
 var global_ID = null;
+// Verbirgt die Matrix Input-Maske
+$(function(){
+	$("#config-filter").hover(function(){
+		$("#matrix-content").fadeIn();
+	});
+	$("#glaett").hover(function(){
+		$("#matrix-content").fadeIn();
+	});
+	$("#diff").hover(function(){
+		$("#matrix-content").fadeIn();
+	});
+	$("#min").hover(function(){
+		$("#matrix-content").fadeOut();
+	});
+	$("#max").hover(function(){
+		$("#matrix-content").fadeOut();
+	});
+	$("#median").hover(function(){
+		$("#matrix-content").fadeOut();
+	});
+	$("#gewMedian").hover(function(){
+		$("#matrix-content").fadeIn();
+	});
+});
 /************************Processing Buttons Action*****************************/
 $(function() {
 	$("#showImg").click(function() {
@@ -17,7 +41,7 @@ $(function() {
 		if(checkIfImageIsUploaded()) {
 			// ID an path: smoothing senden und Histogramm erstellen
 			if(!sendJson("POST", "/smoothing", JSON.stringify({id: global_ID})))
-				return;
+			return;
 			
 			// warten bis Filteroperation angewendet wurde
 			setTimeout(function () { 
@@ -102,9 +126,9 @@ $(function() {
 
 function checkIfImageIsUploaded(){
 	if(global_ID == null) {
-			// Wenn kein Bild hochgeladen wurde
-			$("#img-content").html('<div class="alert alert-danger">Oh Snap! Upload a image first and then select the filter again.</div>');
-			return false;
+		// Wenn kein Bild hochgeladen wurde
+		$("#img-content").html('<div class="alert alert-danger">Oh Snap! Upload a image first and then select the filter again.</div>');
+		return false;
 	} else {
 		return true;
 	}
@@ -164,161 +188,161 @@ $(function() {
 				}
 			}).submit();
 		});
-});
+	});
 		
-function saveImage() {
-	var hiddenIFrameID = 'hiddenDownloader',
-	iframe = document.getElementById(hiddenIFrameID);
-	if (iframe === null) {
-		iframe = document.createElement('iframe');
-		iframe.id = hiddenIFrameID;
-		iframe.style.display = 'none';
-		document.body.appendChild(iframe);
-	}
-	iframe.src = document.getElementById("uploadedImage").src;
-}
-	
-function refreshImage() {
-	window.setTimeout( 
-		function(){
-			var img = document.getElementById("uploadedImage");
-			img.setAttribute('src', img.getAttribute('src') + "?ts=" + new Date().getTime());
-		}, 1000);
-}
-
-function validateMatrix(field) {
-	var x = parseFloat(field.value);
-	console.log("x = " + x + " typeof x = " + typeof x);
-	if (isNaN(x)) {
-		console.log("Bad input: " + field.value);
-		field.value = "";
-		// TODO: add chaning the color of element here, not in getMatrix
-	}
-}
-
-function getMatrix() {
-	var matrix = new Array();
-	var toReturn = true;
-	// r für row c für column
-	for (var i = 0; i < 9; i++){
-		var r = Math.floor(i/3)+1;
-		var c = (i%3)+1;
-		var id = '#r' + r + 'c' + c;
-		matrix[i] = $(id).val();
-		if (matrix[i] == ""){
-			$(id).closest('div').addClass("has-error");
-			toReturn = false;
-		} else {
-			$(id).closest('div').removeClass("has-error");
+	function saveImage() {
+		var hiddenIFrameID = 'hiddenDownloader',
+		iframe = document.getElementById(hiddenIFrameID);
+		if (iframe === null) {
+			iframe = document.createElement('iframe');
+			iframe.id = hiddenIFrameID;
+			iframe.style.display = 'none';
+			document.body.appendChild(iframe);
 		}
-	}
-	return toReturn == true ? matrix : null;
-}
-	
-// sendet die filtermatrix und id des zu bearbeitenden bildes als JSON 
-function sendJson(typ, path, data) {
-	
-	var matrix = getMatrix();
-	if (matrix == null){
-		$('#matrix-error').removeClass("hidden");
-		return false;
-	} else {
-		$('#matrix-error').addClass("hidden");
+		iframe.src = document.getElementById("uploadedImage").src;
 	}
 	
-	// zusammenfügen von array und id
-	data = '[' + data + ', {"r1c1":' + matrix[0] +'},{"r1c2":' + matrix[1] + '}, {"r1c3":' +
-			 matrix[2] + '}, {"r2c1":' + matrix[3] + '}, {"r2c2":' + matrix[4] + '}, {"r2c3":' +
-			  matrix[5] + '}, {"r3c1":' + matrix[6] + '}, {"r3c2":' + matrix[7] + '}, {"r3c3":' + matrix[8] + '}]';	
-	console.log(data);
-	$.ajax({
-		type: typ,
-		data: data,
-		contentType: 'application/json',
-		dataType: 'json',
-		url: path
-	});
-	return true;
-}
-
-function showHistogram(typ, path) {
-	$("#hist-content").fadeIn();
-	var data = new Array(256);
-	var array = new Array(256);
-
-	$.ajax({
-		type: typ,
-		url: path,
-		dataType: "json"
-	}).done(function(jsonData) {
-		for(i in jsonData) {
-			data[i] = jsonData[i];
-			array[i] = "";
+	function refreshImage() {
+		window.setTimeout( 
+			function(){
+				var img = document.getElementById("uploadedImage");
+				img.setAttribute('src', img.getAttribute('src') + "?ts=" + new Date().getTime());
+			}, 1000);
 		}
-		var barChartData = {
-			labels : array,
-			datasets : [
-			{
-				fillColor : "rgba(220,220,220,0.5)",
-				strokeColor : "rgba(220,220,220,1)",
-				data : data
+
+		function validateMatrix(field) {
+			var x = parseFloat(field.value);
+			console.log("x = " + x + " typeof x = " + typeof x);
+			if (isNaN(x)) {
+				console.log("Bad input: " + field.value);
+				field.value = "";
+				// TODO: add chaning the color of element here, not in getMatrix
 			}
-			]
-
 		}
-		var options = {
-			//Boolean - If we show the scale above the chart data			
-			scaleOverlay : true,
-			//Boolean - If we want to override with a hard coded scale
-			scaleOverride : false,
-			//** Required if scaleOverride is true **
-			//Number - The number of steps in a hard coded scale
-			scaleSteps : null,
-			//Number - The value jump in the hard coded scale
-			scaleStepWidth : null,
-			//Number - The scale starting value
-			scaleStartValue : null,
-			//String - Colour of the scale line	
-			scaleLineColor : "rgba(0,0,0,.1)",
-			//Number - Pixel width of the scale line	
-			scaleLineWidth : 1,
-			//Boolean - Whether to show labels on the scale	
-			scaleShowLabels : false,
-			//Interpolated JS string - can access value
-			scaleLabel : "<%=value%>",
-			//String - Scale label font declaration for the scale label
-			scaleFontFamily : "'Arial'",
-			//Number - Scale label font size in pixels	
-			scaleFontSize : 10,
-			//String - Scale label font weight style	
-			scaleFontStyle : "normal",
-			//String - Scale label font colour	
-			scaleFontColor : "#666",	
-			///Boolean - Whether grid lines are shown across the chart
-			scaleShowGridLines : false,
-			//String - Colour of the grid lines
-			scaleGridLineColor : "rgba(0,0,0,.05)",
-			//Number - Width of the grid lines
-			scaleGridLineWidth : 1,	
-			//Boolean - If there is a stroke on each bar	
-			barShowStroke : true,
-			//Number - Pixel width of the bar stroke	
-			barStrokeWidth : 1,
-			//Number - Spacing between each of the X value sets
-			barValueSpacing : 1,
-			//Number - Spacing between data sets within X values
-			barDatasetSpacing : 1,
-			//Boolean - Whether to animate the chart
-			animation : true,
-			//Number - Number of animation steps
-			animationSteps : 60,
-			//String - Animation easing effect
-			animationEasing : "easeOutQuart",
-			//Function - Fires when the animation is complete
-			onAnimationComplete : null
 
+		function getMatrix() {
+			var matrix = new Array();
+			var toReturn = true;
+			// r für row c für column
+			for (var i = 0; i < 9; i++){
+				var r = Math.floor(i/3)+1;
+				var c = (i%3)+1;
+				var id = '#r' + r + 'c' + c;
+				matrix[i] = $(id).val();
+				if (matrix[i] == ""){
+					$(id).closest('div').addClass("has-error");
+					toReturn = false;
+				} else {
+					$(id).closest('div').removeClass("has-error");
+				}
+			}
+			return toReturn == true ? matrix : null;
 		}
-		var myLine = new Chart(document.getElementById("canvas").getContext("2d")).Bar(barChartData, options);
+	
+		// sendet die filtermatrix und id des zu bearbeitenden bildes als JSON 
+		function sendJson(typ, path, data) {
+	
+			var matrix = getMatrix();
+			if (matrix == null){
+				$('#matrix-error').removeClass("hidden");
+				return false;
+			} else {
+				$('#matrix-error').addClass("hidden");
+			}
+	
+			// zusammenfügen von array und id
+			data = '[' + data + ', {"r1c1":' + matrix[0] +'},{"r1c2":' + matrix[1] + '}, {"r1c3":' +
+			matrix[2] + '}, {"r2c1":' + matrix[3] + '}, {"r2c2":' + matrix[4] + '}, {"r2c3":' +
+			matrix[5] + '}, {"r3c1":' + matrix[6] + '}, {"r3c2":' + matrix[7] + '}, {"r3c3":' + matrix[8] + '}]';	
+			console.log(data);
+			$.ajax({
+				type: typ,
+				data: data,
+				contentType: 'application/json',
+				dataType: 'json',
+				url: path
+			});
+			return true;
+		}
 
-	});
-}
+		function showHistogram(typ, path) {
+			$("#hist-content").fadeIn();
+			var data = new Array(256);
+			var array = new Array(256);
+
+			$.ajax({
+				type: typ,
+				url: path,
+				dataType: "json"
+			}).done(function(jsonData) {
+				for(i in jsonData) {
+					data[i] = jsonData[i];
+					array[i] = "";
+				}
+				var barChartData = {
+					labels : array,
+					datasets : [
+					{
+						fillColor : "rgba(220,220,220,0.5)",
+						strokeColor : "rgba(220,220,220,1)",
+						data : data
+					}
+					]
+
+				}
+				var options = {
+					//Boolean - If we show the scale above the chart data			
+					scaleOverlay : true,
+					//Boolean - If we want to override with a hard coded scale
+					scaleOverride : false,
+					//** Required if scaleOverride is true **
+					//Number - The number of steps in a hard coded scale
+					scaleSteps : null,
+					//Number - The value jump in the hard coded scale
+					scaleStepWidth : null,
+					//Number - The scale starting value
+					scaleStartValue : null,
+					//String - Colour of the scale line	
+					scaleLineColor : "rgba(0,0,0,.1)",
+					//Number - Pixel width of the scale line	
+					scaleLineWidth : 1,
+					//Boolean - Whether to show labels on the scale	
+					scaleShowLabels : false,
+					//Interpolated JS string - can access value
+					scaleLabel : "<%=value%>",
+					//String - Scale label font declaration for the scale label
+					scaleFontFamily : "'Arial'",
+					//Number - Scale label font size in pixels	
+					scaleFontSize : 10,
+					//String - Scale label font weight style	
+					scaleFontStyle : "normal",
+					//String - Scale label font colour	
+					scaleFontColor : "#666",	
+					///Boolean - Whether grid lines are shown across the chart
+					scaleShowGridLines : false,
+					//String - Colour of the grid lines
+					scaleGridLineColor : "rgba(0,0,0,.05)",
+					//Number - Width of the grid lines
+					scaleGridLineWidth : 1,	
+					//Boolean - If there is a stroke on each bar	
+					barShowStroke : true,
+					//Number - Pixel width of the bar stroke	
+					barStrokeWidth : 1,
+					//Number - Spacing between each of the X value sets
+					barValueSpacing : 1,
+					//Number - Spacing between data sets within X values
+					barDatasetSpacing : 1,
+					//Boolean - Whether to animate the chart
+					animation : true,
+					//Number - Number of animation steps
+					animationSteps : 60,
+					//String - Animation easing effect
+					animationEasing : "easeOutQuart",
+					//Function - Fires when the animation is complete
+					onAnimationComplete : null
+
+				}
+				var myLine = new Chart(document.getElementById("canvas").getContext("2d")).Bar(barChartData, options);
+
+			});
+		}
